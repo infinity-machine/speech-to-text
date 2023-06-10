4ba5b71612c836964d9fa8bcde5ca9ee7561c487
Connor Carciofini <ccarciofini@gmail.com>
2023-06-10 13:53:34 -0500

    script

--- sr.py
+++ sr.py
@@ -1,13 +1,13 @@
 import speech_recognition as sr
 
-filename = "16-122828-0002.wav"
+filename = "charles-manson.mp3"
 
 r = sr.Recognizer()
 
-# open the file
+# OPEN FILE
 with sr.AudioFile(f'src/{filename}') as source:
-    # listen for the data (load audio to memory)
+    # LOAD AUDIO TO MEMORY
     audio_data = r.record(source)
-    # recognize (convert from speech to text)
+    # CONVERT SPEECH TO TEXT
     text = r.recognize_google(audio_data)
     print(text)
